5b0f94cadac66196b7deb951997a2d94e03ce72a
zivagnor <zivagno.pro@gmail.com>
2026-05-15 03:12:49 +0200

    Ajout du fichier analyse.py - premier programme Python avec liste d'alertes SOC

---
+++ backend/analyse.py
@@ -0,0 +1,30 @@
+alertes=[
+    {"niveau":"Connexion suspecte", "ip": "192.168.1.105", "niveau": "critique"},
+    {"niveau":"Accès non autorisé", "ip": "192.168.1.106", "niveau": "moyen"},
+    {"niveau":"Tentative de brute force", "ip": "192.168.1.107", "niveau": "élevé"},
+    {"niveau":"Activité anormale", "ip": "192.168.1.108", "niveau": "faible"},
+    {"niveau":"Fuite de données", "ip": "192.168.1.109", "niveau": "critique"},
+    {"niveau":"Malware détecté", "ip": "192.168.1.110", "niveau": "élevé"}
+]
+
+
+nc=0
+ne=0
+nm=0
+n=0
+
+for alerte in alertes:
+    if alerte["niveau"] == "critique":
+        print(alerte)
+        nc+=1
+    elif alerte["niveau"] == "élevé":
+        print(alerte)
+        ne+=1
+    elif alerte["niveau"] == "moyen":
+        print(alerte)
+        nm+=1
+    else:
+        print(alerte)
+        n+=1
+
+print(f"Niveau critique: ", nc, "Niveau élevé: ",ne, "Niveau moyen: ", nm, "Niveau faible ou inconnu: ", n)
